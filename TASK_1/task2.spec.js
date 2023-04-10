1130196a68cd0c3bf0c80b8e8626469d7b898dd6
Olga Bogdanova <120654449+olgaabogd@users.noreply.github.com>
2023-04-10 19:25:39 +0300

    Check this file for the second task

--- TASK_1/task2.spec.js
+++ TASK_1/task2.spec.js
@@ -1,1 +1,1 @@
-//*[contains(text(),'SimpleDataset')]/../../td[count(preceding-sibling::td)+1 = count(ancestor::table/thead/tr/th[.='Updated']/preceding-sibling::th)+1]
+ //*[contains(text(),'SimpleDataset')]/../../td[count(preceding-sibling::td)+1 = count(ancestor::table/thead/tr/th[.='Updated']/preceding-sibling::th)+1]
