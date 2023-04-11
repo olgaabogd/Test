0e38b38c8be1c5ee89791e8e52b7a9d31b92ec5c
Olga Bogdanova <o.bahdanavaa@gmail.com>
2023-04-11 21:33:49 +0300

    fix tests

--- TASK_2/tests/myTests.spec.js
+++ TASK_2/tests/myTests.spec.js
@@ -4,10 +4,9 @@ const { test, expect } = require('@playwright/test');
 
 test('locator.fill', async ({ page }) => {
   await page.goto('https://demoqa.com/automation-practice-form');
-  await page.locator('#firstName').fill('Olga');
-  const locator1 = page.locator('#firstName');
-  await expect(locator1).toHaveValue('Olga');
-
+  const locator = page.locator('#firstName');
+  await locator.fill('Olga');
+  await expect(locator).toHaveValue('Olga');
 });
 
 
@@ -16,57 +15,53 @@ test('locator.check.radiobutton', async ({ page }) => {
   const otherText = page.getByText('Other');
   await otherText.check(); 
   await expect(otherText).toBeChecked();
-
 });
 
 
 test('locator.check.checkbox', async ({ page }) => {
   await page.goto('https://demoqa.com/automation-practice-form');
-  await page.getByText('Music').click({force: true});
-  const locator3 = page.getByText('Music')
-  await expect(locator3).toBeChecked();
-
+  const locator = page.getByText('Music');
+  await locator.click({force: true});
+  await expect(locator).toBeChecked();
 });
 
 test('locator.selectOption', async ({ page }) => {
   await page.goto('https://demoqa.com/select-menu');
-  await page.locator('#oldSelectMenu').selectOption('Blue');
-  const locator4 = page.locator('#oldSelectMenu');
-  await expect(locator4).toHaveValue('1');
-
+  const locator = page.locator('#oldSelectMenu');
+  await locator.selectOption('Blue');
+  await expect(locator).toHaveValue('1');
 });
 
 test('locator.click', async ({ page }) => {
   await page.goto('https://demoqa.com/buttons');
   await page.getByText('Double Click Me').dblclick();
-  const locator5 = page.getByText('You have done a double click');
-  await expect(locator5).toBeVisible();
+  const locator = page.getByText('You have done a double click');
+  await expect(locator).toBeVisible();
 
 });
 
 test('locator.hover', async ({ page }) => {
   await page.goto('https://demoqa.com/menu#');
-  await page.getByRole('listitem').filter({ hasText: 'Main Item 1'}).hover();
-  const locator6 = page.getByRole('listitem').filter({ hasText: 'Main Item 1'});
-  await expect(locator6).toHaveCSS('background-color', 'rgb(0, 63, 32)');
+  const element = page.getByRole('listitem');
+  await element.filter({ hasText: 'Main Item 1'}).hover();
+  const locator = element.filter({ hasText: 'Main Item 1'});
+  await expect(locator).toHaveCSS('background-color', 'rgb(0, 63, 32)');
 });
 
 test('locator.setInputFiles', async ({ page }) => {
   const testDoc = 'TASK_2/tests/testDoc.txt';
   await page.goto('https://demoqa.com/upload-download');
   await page.locator('#uploadFile').setInputFiles(testDoc);
-  const locator7 = page.locator('#uploadedFilePath');
-  await expect(locator7).toHaveText('C:\\fakepath\\testDoc.txt');
-
+  const locator = page.locator('#uploadedFilePath');
+  await expect(locator).toHaveText('C:\\fakepath\\testDoc.txt');
 });
 
 
 test('locator.press', async ({ page }) => {
   await page.goto('https://demoqa.com/login');
-  await page.getByPlaceholder('UserName').press('Shift+A');
-  const locator8 = page.getByPlaceholder('UserName');
-  await expect(locator8).toHaveValue('A');
-
+  const locator = page.getByPlaceholder('UserName');
+  await locator.press('Shift+A');
+  await expect(locator).toHaveValue('A');
 });
 
 test('locator.drag&drop', async ({ page }) => {
@@ -76,5 +71,4 @@ test('locator.drag&drop', async ({ page }) => {
   await page.locator('#simpleDropContainer #droppable').hover();
   await page.mouse.up();
   await expect (page.getByText('Dropped')).toBeVisible()
-
 });
