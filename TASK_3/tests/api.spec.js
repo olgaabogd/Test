5679b35b673b7e3bf48f8cd2cbb848d4efdfabd0
olgaabogd <o.bahdanavaa@gmail.com>
2023-04-21 23:09:20 +0300

    check cookies

--- TASK_3/tests/api.spec.js
+++ TASK_3/tests/api.spec.js
@@ -13,3 +13,21 @@ test ('Login', async ({page}) => {
   await expect(locator).toHaveText('Profile');
 
 })
+
+test ('cookies', async ({page}) => {
+  await page.goto('https://demoqa.com/login');
+  await page.getByPlaceholder('UserName').fill('Olga_Bogd');
+  await page.getByPlaceholder('Password').fill('Olga123456789olga@');
+  await page.locator('#login').click();
+
+  await page.waitForURL('https://demoqa.com/profile');
+
+  const cookies = await page.context().cookies();
+  for (const cookie of cookies) {
+    console.log(JSON.stringify(cookie))
+  }
+  await expect(cookies.find(c => c.name == 'userID').value).toBeTruthy();
+  await expect(cookies.find(c => c.name == 'userName').value).toBeTruthy();
+  await expect(cookies.find(c => c.name == 'expires').value).toBeTruthy();
+  await expect(cookies.find(c => c.name == 'token').value).toBeTruthy();
+})
