9157b4de9ee6499c320d791329796d225b33d5c5
olgaabogd <o.bahdanavaa@gmail.com>
2023-04-21 20:34:17 +0300

    login

--- TASK_3/tests/api.spec.js
+++ TASK_3/tests/api.spec.js
@@ -1,9 +1,11 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+import * as credentials from '../infoForTests/credentials.json';
+
 test ('Login', async ({page}) => {
   await page.goto('https://demoqa.com/login');
-  await page.getByPlaceholder('UserName').fill('Olga_Bogd');
-  await page.getByPlaceholder('Password').fill('Olga123456789olga@');
+  await page.getByPlaceholder('UserName').fill(credentials.userName);
+  await page.getByPlaceholder('Password').fill(credentials.password);
   await page.locator('#login').click();
 })
