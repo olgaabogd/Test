9401ced73c6f852871af0d41186fe969ea97729e
Olga Bogdanova <120654449+olgaabogd@users.noreply.github.com>
2023-04-24 20:54:21 +0300

    Comments update

--- TASK_3/tests/api.spec.js
+++ TASK_3/tests/api.spec.js
@@ -3,29 +3,32 @@ const { test, expect } = require('@playwright/test');
 
 import * as credentials from '../infoForTests/credentials.json';
 
-test ('test1Cookies', async ({page}) => {
+test ('testForCookiesAndBookStore', async ({page}) => {
+  //login
   await page.goto('https://demoqa.com/login');
   await page.getByPlaceholder('UserName').fill(credentials.userName);
   await page.getByPlaceholder('Password').fill(credentials.password);
   await page.locator('#login').click();
+  
   const locator = page.locator('.main-header');
   await expect(locator).toHaveText('Profile');
   await page.waitForURL('https://demoqa.com/profile');
 
   const cookies = await page.context().cookies();
+  //cookies check
   for (const cookie of cookies) {
     console.log(JSON.stringify(cookie))
   }
+  
+    //save variables
+  const userID = cookies.find(c => c.name == 'userID').value;
+  const token = cookies.find(c => c.name == 'token').value;
 
   //check that cookies are not empty
   await expect(cookies.find(c => c.name == 'userID').value).toBeTruthy();
   await expect(cookies.find(c => c.name == 'userName').value).toBeTruthy();
   await expect(cookies.find(c => c.name == 'expires').value).toBeTruthy();
   await expect(cookies.find(c => c.name == 'token').value).toBeTruthy();
-
-  //save variables
-  const userID = cookies.find(c => c.name == 'userID').value;
-  const token = cookies.find(c => c.name == 'token').value;
 
   // block images
   await page.route('**/*', (route) => {
