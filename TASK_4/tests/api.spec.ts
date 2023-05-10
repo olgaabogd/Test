b20207132fb1cd1c73326970c1fbdb2fc92cf737
olgaabogd <o.bahdanavaa@gmail.com>
2023-05-10 21:25:46 +0300

    task 4

--- TASK_4/tests/api.spec.ts
+++ TASK_4/tests/api.spec.ts
@@ -1,122 +1,128 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-import * as credentials from '../infoForTests/credentials.json';
-let responsePromise, fullResponse, token, userID, APIresponse;
-let randomNumberOfPages: string;
+import * as credentials from '../infoFor/cred.json'
 
-test ('testFullApi', async ({page, request}) => {
-//login
-  await test.step(`Login`, async () => { 
-  await page.goto('https://demoqa.com/login');
-  await page.getByPlaceholder('UserName').fill(credentials.userName);
-  await page.getByPlaceholder('Password').fill(credentials.password);
-  await page.locator('#login').click();
-  
-  const locator = page.locator('.main-header');
-  await expect(locator).toHaveText('Profile');
-  await page.waitForURL('https://demoqa.com/profile');
-  });
+let responsePromise
+let fullResponse
+let token
+let userID
+let APIresponse
+let randomNumberOfPages: string
+
+test('testFullApi', async ({ page, request }) => {
+  // login
+  await test.step(`Login`, async () => {
+    await page.goto('https://demoqa.com/login')
+    await page.getByPlaceholder('UserName').fill(credentials.userName)
+    await page.getByPlaceholder('Password').fill(credentials.password)
+    await page.locator('#login').click()
+
+    const locator = page.locator('.main-header')
+    await expect(locator).toHaveText('Profile')
+    await page.waitForURL('https://demoqa.com/profile')
+  })
 
   await test.step(`Check that cookies are not empty`, async () => {
-  const cookies = await page.context().cookies();
-//checkin cookies
-  for (const cookie of cookies) {
-    console.log(JSON.stringify(cookie))
-  }
-  
-//save variables
-  userID = cookies.find(c => c.name === 'userID').value;
-  token = cookies.find(c => c.name === 'token').value;
+    const cookies = await page.context().cookies()
+    // checkin cookies
 
-//check that cookies are not empty
-  await expect(cookies.find(c => c.name === 'userID').value).toBeTruthy();
-  await expect(cookies.find(c => c.name === 'userName').value).toBeTruthy();
-  await expect(cookies.find(c => c.name === 'expires').value).toBeTruthy();
-  await expect(cookies.find(c => c.name === 'token').value).toBeTruthy();
-});
+    cookies.forEach((cookie) => console.log(JSON.stringify(cookie)))
+    // save variables
+    userID = cookies.find((c) => c.name === 'userID').value
+    token = cookies.find((c) => c.name === 'token').value
 
-//block images
-  await test.step(`Block all the images`, async () => {
-  await page.route('**/*', (route) => {
-  return route.request().resourceType() === 'image'
-  ? route.abort()
-  : route.continue()
-  
+    // check that cookies are not empty
+    await expect(cookies.find((c) => c.name === 'userID').value).toBeTruthy()
+    await expect(cookies.find((c) => c.name === 'userName').value).toBeTruthy()
+    await expect(cookies.find((c) => c.name === 'expires').value).toBeTruthy()
+    await expect(cookies.find((c) => c.name === 'token').value).toBeTruthy()
   })
-});
 
-  //waiting to intercept a GET request 
+  // block images
+  await test.step(`Block all the images`, async () => {
+    await page.route('**/*', (route) => {
+      return route.request().resourceType() === 'image'
+        ? route.abort()
+        : route.continue()
+    })
+  })
+
+  // waiting to intercept a GET request
   await test.step(`Intercept GET request, make screenshot`, async () => {
-  responsePromise = page.waitForResponse('https://demoqa.com/BookStore/v1/Books');
-  await page.locator('.text:text-is("Book Store")').click();
-  await page.screenshot({path: 'infoForTests/screenshot.png'});
-  });
-  
+    responsePromise = page.waitForResponse(
+      'https://demoqa.com/BookStore/v1/Books'
+    )
+    await page.locator('.text:text-is("Book Store")').click()
+    await page.screenshot({ path: 'infoForTests/screenshot.png' })
+  })
+
   await test.step(`Check GET request`, async () => {
-  const response = await responsePromise;
-  fullResponse = await response.json();
-  expect(response.status()).toBe(200); //check status 200
-  console.log('Response status is ' + response.status()); 
-  const booksAmount = fullResponse.books.length
-  await expect(page.locator(".action-buttons")).toHaveCount(booksAmount); //check that amount of books = UI amount of books
-  });
+    const response = await responsePromise
+    fullResponse = await response.json()
+    expect(response.status()).toBe(200) // check status 200
+    console.log(`Response status is ${response.status()}`)
+    const booksAmount = fullResponse.books.length
+    await expect(page.locator('.action-buttons')).toHaveCount(booksAmount) // check that amount of books = UI amount of books
+  })
 
-//change the number of pages to a random number 
+  // change the number of pages to a random number
   await test.step(`Change the number of pages to a random number `, async () => {
-  randomNumberOfPages = (Math.random() * (1000 - 1) + 1).toString();
-  await page.route( "https://demoqa.com/BookStore/v1/Book?ISBN=*",
-    async (route) => {
-      const response = await route.fetch();
-      let body = await response.text(); //почему-то не работает напрямую с JSON, только через текст. Не очень разобралась в этой части теста
-      const searchBody = JSON.parse(body);
+    randomNumberOfPages = (Math.random() * (1000 - 1) + 1).toString()
+    await page.route(
+      'https://demoqa.com/BookStore/v1/Book?ISBN=*',
+      async (route) => {
+        const response = await route.fetch()
+        let body = await response.text() // почему-то не работает напрямую с JSON, только через текст. Не очень разобралась в этой части теста
+        const searchBody = JSON.parse(body)
 
-      body = body.replace(searchBody.pages, randomNumberOfPages);
-      route.fulfill({
-        response,
-        body,
+        body = body.replace(searchBody.pages, randomNumberOfPages)
+        route.fulfill({
+          response,
+          body,
+          headers: {
+            ...response.headers(),
+          },
+        })
+      }
+    )
+  })
+
+  // click on a random book
+  await test.step(`Click on a random book`, async () => {
+    const rand = Math.floor(Math.random() * fullResponse.books.length)
+    await page.locator('.action-buttons').nth(rand).click()
+  })
+
+  // check that the UI displays exactly the number that was specified earlier
+  await test.step(`Check that the UI displays exactly the number that was specified earlier`, async () => {
+    await expect(page.locator('#pages-wrapper #userName-value')).toHaveText(
+      randomNumberOfPages
+    )
+  })
+
+  // API request
+  await test.step(`Add token`, async () => {
+    APIresponse = await request.get(
+      `https://demoqa.com/Account/v1/User/${userID}`,
+      // add token
+      {
         headers: {
-          ...response.headers(),
+          Authorization: `Bearer ${token}`,
         },
-      });
-    }
-  );
-  });
+      }
+    )
+  })
 
-//click on a random book
-await test.step(`Click on a random book`, async () => {
-let rand = Math.floor(Math.random() * fullResponse.books.length);
-await page.locator(".action-buttons").nth(rand).click();
-});
-
-//check that the UI displays exactly the number that was specified earlier
-await test.step(`Check that the UI displays exactly the number that was specified earlier`, async () => {
-  await expect(page.locator("#pages-wrapper #userName-value")).toHaveText(
-    randomNumberOfPages
-  );
-  });
-
-//API request
-await test.step(`Add token`, async () => {
-  APIresponse = await request.get(`https://demoqa.com/Account/v1/User/${userID}`,
-//add token
-    {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    }
-  );
-});
-
-// check response
-await test.step(`Check response`, async () => {
-  const responseInfo = await APIresponse.json();
-  expect(responseInfo.username).toBe(credentials.userName);
-  expect(responseInfo.books).toEqual([]);
-  if(responseInfo.length > 0) {
-    console.log('There are several books')
-  } else {
+  // check response
+  await test.step(`Check response`, async () => {
+    const responseInfo = await APIresponse.json()
+    expect(responseInfo.username).toBe(credentials.userName)
+    expect(responseInfo.books).toEqual([])
+    if (responseInfo.length > 0) {
+      console.log('There are several books')
+    } else {
       console.log('The number of books is 0')
-  };
-});
-});
+    }
+  })
+})
